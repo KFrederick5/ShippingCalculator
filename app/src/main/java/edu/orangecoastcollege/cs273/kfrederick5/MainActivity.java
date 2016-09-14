3054797dd9a17f54a4e3cfa322555e13a58056e2
kfrederick5 <tefsuncle@gmail.com>
2016-09-14 01:05:40 -0700

    Created the model side of the program

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/MainActivity.java
@@ -2,14 +2,77 @@ package edu.orangecoastcollege.cs273.kfrederick5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static NumberFormat currency =NumberFormat.getCurrencyInstance();
 
+    private TextView addCostText;
+    private TextView package_ounce;
+    private TextView totalCostText;
+    private TextView baseCostText;
+
+    ShippingCost currentBill = new ShippingCost();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addCostText = (TextView) findViewById(R.id.addCost);
+        package_ounce = (TextView) findViewById(R.id.package_ounce);
+        totalCostText = (TextView) findViewById(R.id.totalCost);
+        baseCostText = (TextView) findViewById(R.id.baseCost);
+
+        package_ounce.addTextChangedListener(package_ounceChangedListener);
+
+
+        currentBill.setBaseCost(3.00);
     }
+
+    private TextWatcher package_ounceChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            try
+            {
+                int ounce = s.length() > 0
+                        ?Integer.parseInt(s.toString()): 0;
+                currentBill.setOunce(ounce);
+            }
+            catch(NumberFormatException e)
+            {
+                package_ounce.setText("");
+            }
+
+            package_ounce.setText(currentBill.getOunce());
+            updateViews();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+
+    private  void updateViews()
+    {
+        addCostText.setText(currency.format(currentBill.getAddCost()));
+
+        totalCostText.setText(currency.format(currentBill.getTotalCost()));
+
+        baseCostText.setText(currency.format(currentBill.getBaseCost()));
+    }
+
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ShippingCost.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ShippingCost.java
@@ -1,9 +1,63 @@
 package edu.orangecoastcollege.cs273.kfrederick5;
 
 /**
- * Created by Link on 9/12/2016.
+ * Created by kfrederick5 on 9/12/2016.
  */
 public class ShippingCost {
-    private
+    private int mOunce;
+    private double mTotalCost;
+    private double mAddCost;
+    private double mBaseCost;
 
+    public ShippingCost() {
+    }
+
+    public ShippingCost(int mOunce, double mTotalCost, double mAddCost, double mBaseCost) {
+        this.mOunce = mOunce;
+        this.mTotalCost = mTotalCost;
+        this.mAddCost = mAddCost;
+        this.mBaseCost = mBaseCost;
+    }
+
+    public int getOunce() {
+        return mOunce;
+    }
+
+    public double getTotalCost() {
+        return mTotalCost;
+    }
+
+    public double getBaseCost() {
+        return mBaseCost;
+    }
+
+    public void setOunce(int mOunce) {
+        this.mOunce = mOunce;
+        recalcValues();
+    }
+
+    public void setTotalCost(double mTotalCost) {
+        this.mTotalCost = mTotalCost;
+        recalcValues();
+    }
+
+    public void setAddCost(double mAddCost) {
+        this.mAddCost = mAddCost;
+        recalcValues();
+    }
+
+    public void setBaseCost(double mBaseCost){
+        this.mBaseCost = mBaseCost;
+        recalcValues();
+    }
+
+    public double getAddCost() {
+        return mAddCost;
+    }
+
+    public void recalcValues()
+    {
+        mAddCost = ((mOunce < 16)? 0.0 : (mOunce/4 - 4) * .50);
+        mTotalCost = mAddCost + mBaseCost;
+    }
 }
