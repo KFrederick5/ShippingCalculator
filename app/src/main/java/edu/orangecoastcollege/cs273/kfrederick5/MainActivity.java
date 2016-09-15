488aefd87a617c4df5f4c0fb7269b7440dd5ee67
kfrederick5 <tefsuncle@gmail.com>
2016-09-14 23:09:43 -0700

    Added Java Docs and set currency to US locale

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/MainActivity.java
@@ -1,24 +1,37 @@
 package edu.orangecoastcollege.cs273.kfrederick5;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static NumberFormat currency =NumberFormat.getCurrencyInstance();
+    /**
+     * Set a currency format for dollar amounts.
+     */
+    private static NumberFormat currency =NumberFormat.getCurrencyInstance(Locale.US);
 
 
-
+    /**
+     * Created variables for added cost, total cost, weight, and base cost
+     * that can be manipulated to output different values and
+     * display them to the user.
+     */
     private TextView addCostText;
     private TextView package_ounce;
     private TextView totalCostText;
     private TextView baseCostText;
 
+    /**
+     * Created a new object of type shipping cost to hold values
+     * for the current package being shipped
+     */
     ShippingCost currentBill = new ShippingCost();
 
     @Override
@@ -26,20 +39,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * Hook ups between the XML objects and the variables
+         * substantiated in this java file.
+         */
         addCostText = (TextView) findViewById(R.id.addCost);
-        package_ounce = (TextView) findViewById(R.id.package_ounce);
+        package_ounce = (EditText) findViewById(R.id.package_ounce);
         totalCostText = (TextView) findViewById(R.id.totalCost);
         baseCostText = (TextView) findViewById(R.id.baseCost);
 
+        /**
+         * Created a text listener for the editText item in the XML
+         * document
+         */
         package_ounce.addTextChangedListener(package_ounceChangedListener);
     }
 
+    /**
+     * Created a text watcher to activate if the editText view was
+     * changed by the user.
+     */
     private TextWatcher package_ounceChangedListener = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
         }
 
+        /**
+         * Only used the on text change for instant feedback to the user when
+         * making changes to the editText view
+         * @param s takes in the character sequence enter by the user
+         * @param start reads the string character by character
+         * @param before ?
+         * @param count holds the count for the string array
+         */
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
 
@@ -57,21 +90,21 @@ public class MainActivity extends AppCompatActivity {
             //package_ounce.setText(currentBill.getOunce());
             updateViews();
         }
-
         @Override
         public void afterTextChanged(Editable s) {
-
         }
     };
 
 
+    /**
+     * Sets the TextViews for added cost and total cost with the
+     * values assigned to them in the ShippingCost.java file
+     */
     private  void updateViews()
     {
         addCostText.setText(currency.format(currentBill.getAddCost()));
 
         totalCostText.setText(currency.format(currentBill.getTotalCost()));
-
-
     }
 
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ShippingCost.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/ShippingCost.java
@@ -1,41 +1,84 @@
 package edu.orangecoastcollege.cs273.kfrederick5;
 
 /**
+ * Class created to hold the shipping costs
+ * of an item.
  * Created by kfrederick5 on 9/12/2016.
  */
 public class ShippingCost {
+
+    /**
+     * Created a static variable to hold the base cost for shipping,
+     * an int cost to hold the weight of the package, and doubles
+     * for the added cost and the total cost.
+     */
     private static final double BASE_COST = 3.00;
     private int mOunce;
     private double mTotalCost;
     private double mAddCost;
 
+    /**
+     * Default constructor that assigns the total cost an initial
+     * value equal to the base cost, and the added cost to 0.
+     */
     public ShippingCost() {
         mTotalCost = BASE_COST;
         mAddCost = 0.0;
     }
 
+    /**
+     * Creates an overloaded constructor that passes a weight
+     * value and then recalculates the remaining values based
+     * on that value.
+     * @param mOunce a weight of a package
+     */
     public ShippingCost(int mOunce) {
         this.mOunce = mOunce;
         recalcValues();
     }
 
+    /**
+     * Returns the weight of the package in ounces
+     * @return the weight in ounces
+     */
     public int getOunce() {
         return mOunce;
     }
 
+    /**
+     * Returns the total cost of the package based
+     * on calculations of the weight, base cost, and added
+     * cost.
+     * @return a double of the total shipping cost for the package
+     */
     public double getTotalCost() {
         return mTotalCost;
     }
 
+    /**
+     * Sets the weight of the package in ounces given
+     * by the user. Then recalculates the other values
+     * based on this input.
+     * @param mOunce integer weight of the package in ounces
+     */
     public void setOunce(int mOunce) {
         this.mOunce = mOunce;
         recalcValues();
     }
 
+    /**
+     *Returns the added cost of the package calculated by
+     * taking the weight and adding .50 every 4 ounces above 16
+     * @return a double that returns the added cost for the package
+     */
     public double getAddCost() {
         return mAddCost;
     }
 
+    /**
+     * Calculates the values of total and added cost based on the
+     * ounces given by the user and the base cost of shipping.
+     */
     public void recalcValues()
     {
         mAddCost = ((mOunce < 16)? 0.0 : Math.ceil(mOunce/4.0 - 4) * .50);
